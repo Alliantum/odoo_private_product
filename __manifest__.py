d2ce72e80c868f499cdba029a137e516969a5f03
Iago Alonso <iago.alonso@opensolutions.net>
2020-12-23 16:52:12 +0100

    Update for public release

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Brand Partner Products",
+    'summary': """Restricted (private) products for Customers""",
+    'description': """
+        Create a relation between a product and a customer, and avoid selling that products to any other customer.
+    """,
+    'author': "Alliantum",
+    'website': "https://www.alliantum.com",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'category': 'Inventory',
+    'depends': ['base', 'product', 'sale', 'odoo_invoice_addresses'],
+    'data': [
+        'views/product_template.xml',
+        'views/product_variant_view.xml',
+        'views/res_partner.xml',
+    ]
+}
---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from . import account_invoice
+from . import product_product
+from . import product_template
+from . import res_partner
+from . import sale_order
